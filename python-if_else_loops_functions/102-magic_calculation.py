fdb76da74c969f99f6a7cceadf2cce500358be73
Nino-Of-Tech <nwachukwunino15@gmail.com>
2022-07-05 15:06:05 -0700

    new

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-def magic_calculation(a, b, c):
+
+def magic_calculation(a,b,c):
     """Match bytecode provided by Holberton School."""
     if a < b:
         return (c)
     if c > b:
         return (a + b)
-    return (a*b - c)
+    return (a*b - c) 
